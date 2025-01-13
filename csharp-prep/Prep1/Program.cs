0da4635e5d3beae90ad842a574da55a1e5d5cb4e
brookelin33 <156544038+brookelin33@users.noreply.github.com>
2025-01-13 15:41:23 -0700

    prep1/2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage?");
+        string percentage = Console.ReadLine();
+        if (int.Parse(percentage) >= 90)
+        {
+            Console.WriteLine("You have an A.");
+        }
+        else if (int.Parse(percentage) >= 80)
+        {
+            Console.WriteLine("You have a B.");
+        }
+        else if (int.Parse(percentage) >= 70)
+        {
+            Console.WriteLine("You have a C.");
+        }
+        else if (int.Parse(percentage) >= 60)
+        {
+            Console.WriteLine("You have a D.");
+        }
+        else if (int.Parse(percentage) < 60)
+        {
+            Console.WriteLine("You have a F.");
+        }
+        if (int.Parse(percentage) >= 70)
+        {
+            Console.WriteLine("You have passed the course. Congradulations!");
+        }
+        else if (int.Parse(percentage) <70)
+        {
+            Console.WriteLine("You did not pass the class. You'll get it next time!");
+        }
+
     }
 }
