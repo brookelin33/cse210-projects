68dddd2c90382c32c41fdb2d0a1750e04580b28e
brookelin33 <156544038+brookelin33@users.noreply.github.com>
2025-01-22 19:51:19 -0700

    job and resume c

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        
+        // Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+
+        string guess = "";
+
+        do{
+        Console.Write("What is your guess? ");
+        guess = Console.ReadLine();
+        
+        if (int.Parse(guess) > int.Parse(magicNumber))
+        {
+            Console.WriteLine("Guess lower. ");
+        }
+        else if (int.Parse(guess) < int.Parse(magicNumber))
+        {
+            Console.WriteLine("Guess higher. ");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it! ");
+        }
+        } 
+        while (int.Parse(guess) != int.Parse(magicNumber));
 }
+    } 
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0
+        List<int> numbers = new List<int>();
+
+        int number = 0;
+        // Console.WriteLine("Write a number to add to the list. Type '0' to quit. ");
+        // string usernumber = Console.ReadLine();
+        // int number = int.Parse(usernumber);
+
+        do  {
+            Console.WriteLine("Write a number to add to the list. Type '0' to quit. ");
+            string usernumber = Console.ReadLine();
+            number = int.Parse(usernumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
     }
+        
 }
+    //     Compute the sum, or total, of the numbers in the list.
+    //     Compute the average of the numbers in the list.
+    //     Find the maximum, or largest, number in the list.
+    // 
+    
